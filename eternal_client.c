fb7810d9ac28e454a144ca77c6ade637ba192474
Dileep PJ <pjdileep6@gmail.com>
2019-04-23 11:37:38 +0530

    commit #2 - * Able to communicate to server with multiple clients * Properly closing connection

--- eternal_client.c
+++ eternal_client.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <signal.h>
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <mqueue.h>
@@ -16,14 +17,20 @@
 
 time_t now;
 
+mqd_t qd_server, qd_client;   // queue descriptors
+char client_queue_name [64];
+
+void sig_handler(int signum) {
+    printf("\n[-] Exiting Application\n");
+    mq_close(qd_server);
+    mq_close(qd_client);
+    mq_unlink(client_queue_name);
+    exit(0);
+}
+
 int main (int argc, char **argv)
 {
-    char client_queue_name [64];
-    mqd_t qd_server, qd_client;   // queue descriptors
 
-
-    // create the client queue for receiving messages from server
-    // sprintf (client_queue_name, "/eternal-client-%d", getpid ());
     if(strcmp(argv[1],"CONNECT") || argc != 3 ){
       perror("[-] Error : Pass with arguments. eg. ./eternal_client CONNECT <client_id> ");
       exit(1);
@@ -31,71 +38,69 @@ int main (int argc, char **argv)
     sprintf (client_queue_name, "/eternal-client-%s", argv[2]);
 
     struct mq_attr attr;
-
     attr.mq_flags = 0;
     attr.mq_maxmsg = MAX_MESSAGES;
     attr.mq_msgsize = MAX_MSG_SIZE;
     attr.mq_curmsgs = 0;
 
+    // SIGNAL INTERRUPTION
+    signal(SIGINT, sig_handler);  // CTRL+C
+
+
+    // STARTING THE CLIENT NODE
     if ((qd_client = mq_open (client_queue_name, O_RDONLY | O_CREAT, QUEUE_PERMISSIONS, &attr)) == -1) {
-        perror ("Client: mq_open (client)");
+        perror ("[-] Client : mq_open (client)");
+        exit (1);
+    }
+    // STARTING THE SERVER NODE
+    if ((qd_server = mq_open (SERVER_QUEUE_NAME, O_WRONLY)) == -1) {
+        perror ("[-] Client : mq_open (server)");
         exit (1);
     }
 
-    if ((qd_server = mq_open (SERVER_QUEUE_NAME, O_WRONLY)) == -1) {
-        perror ("Client: mq_open (server)");
-        exit (1);
-    }
 
     char in_buffer [MSG_BUFFER_SIZE];
-
-    printf ("Data to be sent [Position & velocity] : ");
-
     char temp_buf[MAX_MSG_SIZE];
 
 
-    while (fgets (temp_buf, MAX_MSG_SIZE, stdin)) {
 
-        // send message to server
+    // printf ("Data to be sent [Position & velocity] : ");
+    printf ("[=] Client [pos,vel] : ");
+    while (fgets (temp_buf, MAX_MSG_SIZE, stdin)) {
+        // Checking for STOP
+        if(strcmp(temp_buf,"STOP\n") == 0){
+          sig_handler(2);
+        }
+
+        // send queue name to server
         if (mq_send (qd_server, client_queue_name, strlen (client_queue_name) + 1, 0) == -1) {
-            perror ("Client: Not able to send message to server");
+            perror ("[-] Client : Not able to send message to server");
             continue;
         }
 
-        // time stamp
+        // time stamp TODO
         // char buf[16];
         // snprintf(buf, 16, "%lu", time(NULL));
         // strcat(temp_buf,buf);
 
-        //Send data - [position,velocity]
+        //Send data - [position,velocity] - data in temp_buf
         if (mq_send (qd_server, temp_buf, strlen (temp_buf) + 1, 0) == -1) {
-            perror ("Client: Not able to send message to server");
+            perror ("[-] Client : Not able to send message to server");
             continue;
         }
 
-        // receive response from server
-
+        // receive modified response to the data from server - data goes to in_buffer
         if (mq_receive (qd_client, in_buffer, MSG_BUFFER_SIZE, NULL) == -1) {
-            perror ("Client: mq_receive");
-            exit (1);
+            perror ("[-] Server : Message error");
+            sig_handler(2);
+            // exit (1);
         }
-        // display token received from server
-        printf ("Server: %s\n\n", in_buffer);
 
-        printf ("Data to be sent [Position & velocity] : ");
+        //Print the message on screen
+        printf ("[.] Server : message received.\n");
+        printf("[.] Message : %s\n", in_buffer);
+        printf ("[=] Client [pos,vel] : ");
     }
-
-
-    if (mq_close (qd_client) == -1) {
-        perror ("Client: mq_close");
-        exit (1);
-    }
-
-    if (mq_unlink (client_queue_name) == -1) {
-        perror ("Client: mq_unlink");
-        exit (1);
-    }
-    printf ("Client: bye\n");
 
     exit (0);
 }
--- eternal_server.c
+++ eternal_server.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
-
+#include <signal.h>
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <mqueue.h>
@@ -13,52 +13,62 @@
 #define MAX_MSG_SIZE 256
 #define MSG_BUFFER_SIZE MAX_MSG_SIZE + 10
 
+mqd_t qd_server, qd_client;   // queue descriptors
+
+void sig_handler(int signum) {
+    printf("[-] Received SIGINT. Exiting Application\n");
+    mq_close(qd_server);
+    mq_close(qd_client);
+    mq_unlink(SERVER_QUEUE_NAME);
+    exit(0);
+}
+
 int main (int argc, char **argv)
 {
-    mqd_t qd_server, qd_client;   // queue descriptors
-    long token_number = 1; // next token to be given to client
-
-    printf ("Server: Hello, World!\n");
 
     struct mq_attr attr;
-
     attr.mq_flags = 0;
     attr.mq_maxmsg = MAX_MESSAGES;
     attr.mq_msgsize = MAX_MSG_SIZE;
     attr.mq_curmsgs = 0;
 
+    printf ("[.] Server : Waiting for Client Messages\n");
+    // STARTING THE SERVER NODE
     if ((qd_server = mq_open (SERVER_QUEUE_NAME, O_RDONLY | O_CREAT, QUEUE_PERMISSIONS, &attr)) == -1) {
-        perror ("Server: mq_open (server)");
+        perror ("[-] Server : mq_open (server)");
         exit (1);
     }
     char in_buffer [MSG_BUFFER_SIZE];
     char out_buffer [MSG_BUFFER_SIZE];
     char delim[] = ",";
     char temp[MSG_BUFFER_SIZE] = {0};
+
+    // SIGNAL INTERRUPTION
+    signal(SIGINT, sig_handler);  // CTRL+C
+
     while (1) {
-        // get the oldest message with highest priority
+        // Receiving the client message queue name (in in_buffer) to communicate
         if (mq_receive (qd_server, in_buffer, MSG_BUFFER_SIZE, NULL) == -1) {
-            perror ("Server: mq_receive");
+            perror ("[-] Server : mq_receive");
             exit (1);
         }
 
-        printf ("\nServer: message received from %s \n",in_buffer);
-
-        // send reply message to client
-
+        // Open lient message queue
         if ((qd_client = mq_open (in_buffer, O_WRONLY)) == 1) {
-            perror ("Server: Not able to open client queue");
+            perror ("[-] Server : Not able to open client queue");
             continue;
         }
+        printf("[+] Connection established to : %s\n", in_buffer);
 
         // Receive data
-        // get the oldest message with highest priority
         if (mq_receive (qd_server, in_buffer, MSG_BUFFER_SIZE, NULL) == -1) {
-            perror ("Server: mq_receive");
+            perror ("[-] Server : Message error");
             exit (1);
         }
 
-        printf ("Message :  %s",in_buffer);
+        //Print the message on screen
+        printf ("[+] Server : Message received from client.\n");
+        printf ("[+] Message :  %s",in_buffer);
         //TO EXTRACT THE DATA {POS & VEL}
         char *ptr = strtok(in_buffer, delim);
         memset(&temp, 0, sizeof(temp));
@@ -72,14 +82,12 @@ int main (int argc, char **argv)
         sprintf (out_buffer, "Modified : %s", temp);
         printf("[.] Modified Message : %s\n", out_buffer);
 
-        // sprintf (out_buffer, "%ld", token_number);
-
+        // Sending modified message to client
         if (mq_send (qd_client, out_buffer, strlen (out_buffer) + 1, 0) == -1) {
-            perror ("Server: Not able to send message to client");
+            perror ("[-] Server : Not able to send message to client");
             continue;
         }
 
-        printf ("Server: response sent to client.\n");
-        token_number++;
+        printf ("[-] Server : response sent to client.\n");
     }
 }
